09af61348192757cbd54c474ac9ce0875dc7e7a7
AnonyCat <jacobsfer@hotmail.com>
2017-08-07 22:18:49 +0200

    Added support for NEO

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ let amt = 0;
  * All data about ETH price comes from Coinbase API.
  */
 const COINBASE_ETH_PRICE_URL = "https://api.coinbase.com/v2/prices/ETH-USD/spot"
+const BITTREX_NEO_PRICE_URL = "https://bittrex.com/api/v1.1/public/getticker?market=USDT-NEO"
 
 /**
  * setBadge
@@ -32,14 +33,36 @@ const sendPriceMsg = (amt) => {
   });
 };
 
+
 /**
- * fetchPrice
+ * fetchNeoPrice
  * ----------
  * Fetches current ETH price from Coinbase API.
  * Sets badge number to price of ETH.
  * Sends price data to popup.js to display in the extension popup.
  */
-const fetchPrice = () => {
+const fetchNeoPrice = () => {
+  xhr.open("GET", BITTREX_NEO_PRICE_URL, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      const resp = JSON.parse(xhr.responseText);
+      const { result: { last } } = resp;
+      amt = last;
+      setBadge(last);
+      sendPriceMsg(last);
+    }
+  }
+  xhr.send();
+};
+
+/**
+ * fetchEthPrice
+ * ----------
+ * Fetches current ETH price from Coinbase API.
+ * Sets badge number to price of ETH.
+ * Sends price data to popup.js to display in the extension popup.
+ */
+const fetchEthPrice = () => {
   xhr.open("GET", COINBASE_ETH_PRICE_URL, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
@@ -67,9 +90,9 @@ chrome.runtime.onMessage.addListener(function (msg, sender, response) {
 /**
  * Init
  */
-fetchPrice();
+fetchNeoPrice();
 
 /**
  * Poll for prices every 5 seconds.
  */
-setInterval(fetchPrice, 20000);
+setInterval(fetchNeoPrice, 20000);
